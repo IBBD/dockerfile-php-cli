dfceaf8917dde79de5224674b0fdb58bd90a92f4
alex cai <cyy0523xc@gmail.com>
2015-10-05 03:07:33 +0800

    run sh

--- php-cli-dev/run.sh
+++ php-cli-dev/run.sh
@@ -5,7 +5,7 @@ name=php-cli-dev
 sudo docker stop ibbd-$name
 sudo docker rm ibbd-$name
 
-sudo docker --debug run -ti --name --rm ibbd-$name \
+sudo docker --debug run -ti --rm --name ibbd-$name \
     -v /var/www:/var/www \
     ibbd/$name \
     /bin/bash
